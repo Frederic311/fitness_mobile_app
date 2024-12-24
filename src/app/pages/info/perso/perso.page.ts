40fb9d1beaef0d9887a3f386e8b87e9094854e33
Youmbi-yvan4 <ngongang.yvan@institutsaintjean.org>
2024-12-24 14:21:24 +0100

    splash

--- src/app/pages/info/perso/perso.page.ts
+++ src/app/pages/info/perso/perso.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -17,18 +18,24 @@ export class PersoPage {
   profilePicture: string | null = null; // Holds the preview image URL
   showPassword = false;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
-  // Method to handle photo selection and show preview
+  // Method to handle photo selection and preview
   onPhotoSelected(event: any) {
     const file = event.target.files[0];
     if (file) {
       const reader = new FileReader();
       reader.onload = () => {
-        this.profilePicture = reader.result as string; // Set the preview image URL
+        this.profilePicture = reader.result as string; // Set preview image URL
       };
-      reader.readAsDataURL(file); // Read the file as a Data URL
+      reader.readAsDataURL(file); // Read the file as Data URL
     }
+  }
+
+  // Method to programmatically trigger the file input
+  triggerFileInput() {
+    const fileInput = document.getElementById('fileInput') as HTMLElement;
+    fileInput.click();
   }
 
   // Method to toggle password visibility
@@ -38,13 +45,15 @@ export class PersoPage {
 
   // Form submission handler
   submitForm() {
-    console.log('Form submitted with:', this.profile, 'Profile Picture:', this.profilePicture);
-    // Logic to submit form data (e.g., to a backend API)
+    // console.log('Form submitted with:', this.profile, 'Profile Picture:', this.profilePicture);
+    this.router.navigate(['/role'])
+    console.log('Profile creation skipped');
+    // Submit form data (e.g., to a backend API)
   }
 
   // Skip profile creation
   skipProfile() {
     console.log('Profile creation skipped');
-    // Redirect to another page, such as the home screen
+    // Redirect to another page (e.g., the home screen)
   }
 }
